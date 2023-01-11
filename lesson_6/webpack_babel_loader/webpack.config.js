7b7c4ff79a2334fb989bf4bf6471e20765a5a51d
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-01-11 10:55:49 +0200

    add

--- lesson_6/webpack_babel_loader/webpack.config.js
+++ lesson_6/webpack_babel_loader/webpack.config.js
@@ -1,12 +1,15 @@
+const path = require("path");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/i,
         use: ["babel-loader"],
       },
     ],
